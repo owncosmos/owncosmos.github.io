fd3cf4cfca4ccd7129fe0b32288bc62acc5c7d10
Sergei Ivanov <100bb100hands@gmail.com>
2021-01-25 15:39:30 +0300

    add header

--- src/main/index.js
+++ src/main/index.js
@@ -1,16 +1,17 @@
 import './index.css'
-
+import Header from '../components/header'
 function Main() {
     return (
-        <>
-            <h1 className="header">здесь скоро будет мое портфолио/блог</h1>
+        <div className="container">
+            <Header/>
+            <h1>здесь скоро будет мое портфолио/блог</h1>
             <div>
                 <a href="https://github.com/codebysergeyivanov/MusicApp">https://github.com/codebysergeyivanov/MusicApp</a>
             </div>
             <div>
                 <a href="https://github.com/codebysergeyivanov/Swift.MyFavoritePlaces">https://github.com/codebysergeyivanov/Swift.MyFavoritePlaces</a>
             </div>
-        </>
+        </div>
     );
 }
 
